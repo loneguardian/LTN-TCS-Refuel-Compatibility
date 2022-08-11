f5450bf2aa062f46cb110a922d7d38febc9602dd
Xeraph <teckpei@gmail.com>
2022-08-11 19:26:17 +0800

    code cleanup, removed first prototype

--- control.lua
+++ control.lua
@@ -1,15 +1,15 @@
+-- require
 require "util"
 
 -- parameters (to be changed to settings)
 local refuel_station_name = "TestStation [virtual-signal=refuel-signal]"
 local refuel_station_inactivity_condition_timeout = 300 -- ticks
 
--- detect debug
+-- debug detection
 local debug = (__DebugAdapter and true) or false
 
 -- local variables
---local getDeliveryStart -- key: train ID, value: started (tick)
-local trains = {} -- key: "unique" ID, value: LuaTrain
+local trains = {} -- key: "unique" train ID, value: LuaTrain object
 
 local wait_condition = {
     compare_type = "and",
@@ -40,7 +40,6 @@ local function initialiseTrains()
 end
 
 -- handlers
-
 local function on_configuration_changed(event)
     -- remove obsolete globals
     global.getDeliveryStart = nil
@@ -66,7 +65,7 @@ local function on_train_created(event)
     if event.old_train_id_1 then trains[event.old_train_id_1] = nil end
     if event.old_train_id_2 then trains[event.old_train_id_2] = nil end
 
-    -- create new entry with LuaTrain
+    -- create new entry
     trains[event.train.id] = event.train
 
     if debug then print(
@@ -78,46 +77,9 @@ local function on_train_created(event)
     ) end
 end
 
---[[ dispatcher updates firing too frequently, ideal event will be a train specific event when a new delivery is scheduled
-
-local function on_dispatcher_updated(event)
-    -- event received, check if new delivery
-    local isNewDelivery = false
-    local d = event.deliveries
-    
-    for k, v in pairs(d) do
-
-        isNewDelivery = false
-
-        if getDeliveryStart[k] then
-            -- train ID exist
-            -- check `started`
-            if d[k].started > getDeliveryStart[k] then
-                --event tick is higher than existing -> new delivery
-                isNewDelivery = true
-            end
-        else
-            -- new train ID
-            isNewDelivery = true
-        end
-
-        if isNewDelivery then
-            getDeliveryStart[k] = d[k].started
-            if debug then print(on_dispatcher_updated(): event.tick, "new d", k, d[k].started) end
-        end
-    end
-        -- check train ID, if not exist then true
-        -- check started, if larger than existing dictionary value then true
-        -- store train ID with current started value
-
-    -- if new delivery then add another station to end of schedule
-end
-]]--
-
 local function registerEvents()
     if remote.interfaces["logistic-train-network"] then
         script.on_event(remote.call("logistic-train-network", "on_delivery_pickup_complete"), on_delivery_pickup_complete)
-        --script.on_event(remote.call("logistic-train-network", "on_dispatcher_updated"), on_dispatcher_updated)
     end
 
     script.on_configuration_changed(on_configuration_changed)
@@ -128,13 +90,10 @@ script.on_load(function ()
     registerEvents()
 
     initialiseTrains()
-    --getDeliveryStart = global.getDeliveryStart
 end)
 
 script.on_init(function ()
     registerEvents()
 
     initialiseTrains()
-    --global.getDeliveryStart = {}
-    --getDeliveryStart = global.getDeliveryStart
 end)
