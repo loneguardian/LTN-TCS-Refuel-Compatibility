64d0edbe19fa2d1acfc6692db49ecab76cd10b20
Xeraph <teckpei@gmail.com>
2022-08-13 21:53:47 +0800

    rewrote trains dictionary into trainById

--- control.lua
+++ control.lua
@@ -1,55 +1,95 @@
--- require
-require "util"
-
 -- parameters (to be changed to settings)
-local refuel_station_name = "TestStation [virtual-signal=refuel-signal]"
-local refuel_station_inactivity_condition_timeout = 300 -- ticks
+local setting_refuel_station_name = "TestStation [virtual-signal=refuel-signal]"
+local setting_refuel_station_inactivity_condition_timeout = 300 -- ticks
 
 -- debug detection
 local debug = (__DebugAdapter and true) or false
 
+-- require
+local util = require "util"
+
+-- localise table functions
+local table = {
+    deepcopy = util.table.deepcopy,
+    insert = table.insert
+}
+
+-- event IDs
+local on_train_created = defines.events.on_train_created
+
 -- local variables
-local trains = {} -- key: "unique" train ID, value: LuaTrain object
+local trainById -- global - key: "unique" train ID, value: LuaTrain object
 
 local wait_condition = {
     compare_type = "and",
     type = "inactivity",
-    ticks = refuel_station_inactivity_condition_timeout
+    ticks = setting_refuel_station_inactivity_condition_timeout
 }
 
 local refuel_station_record = {
-    station = refuel_station_name,
+    station = setting_refuel_station_name,
     wait_conditions = {wait_condition}
 }
 
 -- local functions
-local function initialiseTrains()
+local function initialiseTrainById()
+    global.trainById = {}
+
+    trainById = global.trainById
 
     local trainArray
 
-    for _, v in pairs(game.forces) do
-        trainArray = v.get_trains()
+    for _, f in pairs(game.forces) do
+        trainArray = f.get_trains()
 
-        for _, v in ipairs(trainArray) do
-            trains[v.id] = v
+        for _, t in ipairs(trainArray) do
+            trainById[t.id] = t
         end
 
     end
 
-    if debug then print("initialiseTrains(): initialised train dictionary") end
+    if debug then print("updateTrainById(): initialised train dictionary") end
+end
+
+local function updateTrainById(event)
+    if not event then return end
+        
+    if event.name == on_train_created then
+        -- update trainById dictionary
+        -- clear old IDs in dictionary
+        if event.old_train_id_1 then trainById[event.old_train_id_1] = nil end
+        if event.old_train_id_2 then trainById[event.old_train_id_2] = nil end
+
+        -- create new entry
+        trainById[event.train.id] = event.train
+
+        if debug then
+            print(
+                "updateTrainById(): by on_train_created. Old IDs:",
+                event.old_train_id_1,
+                event.old_train_id_2,
+                "New ID",
+                event.train.id
+            )
+        end
+    end
+
+    -- TODO: to handle train ID that got deleted? (currently not necessary but probably good if we can find an event to remove invalid trains)
 end
 
 -- handlers
 local function on_configuration_changed(event)
     -- remove obsolete globals
     global.getDeliveryStart = nil
-    global.trains = nil
+    
+    initialiseTrainById()
 end
 
 local function on_delivery_pickup_complete(event)
     if debug then print("on_delivery_pickup_complete():", event.tick, "pickup complete", event.train_id) end
 
-    local t = trains[event.train_id]
+    -- insert new stop for train announcing delivery_pickup_complete
+    local t = event.train_id and trainById[event.train_id]
 
     if t.valid then
         local sch = table.deepcopy(t.schedule)
@@ -61,39 +101,28 @@ local function on_delivery_pickup_complete(event)
 end
 
 local function on_train_created(event)
-    -- clear old IDs in dictionary
-    if event.old_train_id_1 then trains[event.old_train_id_1] = nil end
-    if event.old_train_id_2 then trains[event.old_train_id_2] = nil end
-
-    -- create new entry
-    trains[event.train.id] = event.train
-
-    if debug then print(
-        "on_train_created(): train dictionary updated. Old IDs:",
-        event.old_train_id_1,
-        event.old_train_id_2,
-        "New ID",
-        event.train.id
-    ) end
+    updateTrainById(event)
 end
 
 local function registerEvents()
+    script.on_configuration_changed(on_configuration_changed)
+
+    script.on_event(defines.events.on_train_created, on_train_created)
+
     if remote.interfaces["logistic-train-network"] then
         script.on_event(remote.call("logistic-train-network", "on_delivery_pickup_complete"), on_delivery_pickup_complete)
     end
-
-    script.on_configuration_changed(on_configuration_changed)
-    script.on_event(defines.events.on_train_created, on_train_created)
 end
 
 script.on_load(function ()
     registerEvents()
 
-    initialiseTrains()
+    -- localise globals
+    trainById = global.trainById
 end)
 
 script.on_init(function ()
     registerEvents()
 
-    initialiseTrains()
+    initialiseTrainById()
 end)
