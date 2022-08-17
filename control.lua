983d2edbd8d3a406622716ffbfe4bca26eca3852
Xeraph <teckpei@gmail.com>
2022-08-18 03:54:32 +0800

    code cleanup

--- control.lua
+++ control.lua
@@ -11,8 +11,19 @@ local table = {
 }
 
 -- local variables
-local setting_refuel_station_name
-local setting_refuel_station_inactivity_condition_timeout
+local setting_refuel_station_name = settings.global["tcsltn-refuel-station-name"].value
+local setting_refuel_station_inactivity_condition_timeout = settings.global["tcsltn-refuel-station-inactivity-condition-timeout"].value * 60
+
+local wait_condition = {
+    compare_type = "and",
+    type = "inactivity",
+    ticks = setting_refuel_station_inactivity_condition_timeout
+}
+
+local refuel_station_record = {
+    station = setting_refuel_station_name,
+    wait_conditions = {wait_condition}
+}
 
 -- local functions
 local function checkLtnSetting()
@@ -21,45 +32,20 @@ local function checkLtnSetting()
     local isResetAtRequester = s.value
     local settingName = game.mod_setting_prototypes["ltn-dispatcher-requester-delivery-reset"].localised_name
 
-    if debug then print("LTN setting changed - reset at requester: ", isResetAtRequester) end
+    if debug then print("LTN setting", settingName, isResetAtRequester) end
 
     if isResetAtRequester then
         game.print({"console-message.ltn-setting-warning", settingName})
     end
 end
 
-local function loadSettings()
-    setting_refuel_station_name = settings.global["tcsltn-refuel-station-name"].value
-    setting_refuel_station_inactivity_condition_timeout = settings.global["tcsltn-refuel-station-inactivity-condition-timeout"].value * 60
-end
-
-local function getRefuelStationRecord()
-    local wait_condition = {
-        compare_type = "and",
-        type = "inactivity",
-        ticks = setting_refuel_station_inactivity_condition_timeout
-    }
-
-    local refuel_station_record = {
-        station = setting_refuel_station_name,
-        wait_conditions = {wait_condition}
-    }
-
-    return refuel_station_record
-end
-
 -- handlers
 local function on_configuration_changed(event)
-    -- remove obsolete globals
-    global.getDeliveryStart = nil
-    global.trainById = nil
-
     checkLtnSetting()
-    loadSettings()
 end
 
 local function on_delivery_pickup_complete(event)
-    if debug then print("on_delivery_pickup_complete():", event.tick, "pickup complete", event.train_id) end
+    if debug then print("on_delivery_pickup_complete():", event.tick, "pickup complete", event.train_id) end --test
 
     -- insert new stop for train announcing delivery_pickup_complete
     local t = event.train
@@ -67,24 +53,24 @@ local function on_delivery_pickup_complete(event)
     if t.valid then
         local sch = table.deepcopy(t.schedule)
         
-        table.insert(sch.records, getRefuelStationRecord())
+        table.insert(sch.records, refuel_station_record)
 
         t.schedule = sch
     end
 end
 
 local function on_setting_changed(event)
-
     local settingName = event.setting
 
     if settingName == "ltn-dispatcher-requester-delivery-reset" then
         checkLtnSetting()
     elseif settingName == "tcsltn-refuel-station-name" then
         setting_refuel_station_name = settings.global[settingName].value
+        refuel_station_record.station = setting_refuel_station_name
     elseif settingName == "tcsltn-refuel-station-inactivity-condition-timeout" then
         setting_refuel_station_inactivity_condition_timeout = settings.global[settingName].value * 60
+        wait_condition.ticks = setting_refuel_station_inactivity_condition_timeout
     end
-
 end
 
 -- register handlers
@@ -99,12 +85,10 @@ end
 
 script.on_load(function ()
     registerCondEvents()
-    checkLtnSetting()
 end)
 
 -- init
 script.on_init(function ()
     registerCondEvents()
-    loadSettings()
     checkLtnSetting()
 end)
